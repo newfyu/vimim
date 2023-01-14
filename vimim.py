e8285cc574c779aecd1474f083b54eb353d63368
lhan <lhan@192.168.31.44>
2023-01-14 14:40:36 +0800

    fix bug

--- vimim.py
+++ vimim.py
@@ -14,15 +14,16 @@ def is_abc():
 
 def on_press(key):
     global last_esc_press_time
-    if key == keyboard.Key.esc and not is_abc():
+    if key == keyboard.Key.esc and is_abc()=='':
         current_time = time.time()
         if last_esc_press_time is not None and current_time - last_esc_press_time < 0.5:
             ctl.press(Key.ctrl)
             ctl.press(Key.space)
-            time.sleep(0.1)
+            #  time.sleep(0.01)
             ctl.release(Key.ctrl)
             ctl.release(Key.space)
-            print('快速按下esc键')
+            current_time = None
+            #  print('快速按下esc键')
         last_esc_press_time = current_time
 
 def on_release(key):
